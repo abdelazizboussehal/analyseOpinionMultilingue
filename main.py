cfbd3511e251d5fedf87adc3104ae4db99b9e3ed
boussehal abdelaziz <abdelaziz.boussehal@univ-constantine2.dz>
2020-03-10 11:59:12 +0100

    ajouter entete

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
+'''
+Proposition d’un pivot interlangue (Ar-Fr-Ang) pour la traduction automatique afin d’aider à l’analyse d’opinion
+@group: Abdelaziz boussehal, Abderraouf Mahmoudi
+@date: 2019-2020
+'''
+
 from textblob import TextBlob
 from os import path as os_path
 from CreationModele import CreationModele
 from AnalyseModeles import AnalyseModeles
+
+
 """ module creation """
 cr = CreationModele()
-cr.lecture()
+cr.lecture("text.txt")
 cr.preTraitement()
 cr.creationModele()
+
 """ module analyse """
 analyse = AnalyseModeles("Python")
 analyse.lectureModee()
