abb5c6ebe11e89c41f8a53a7ed9fd203b9ab571a
boussehal abdelaziz <abdelaziz.boussehal@univ-constantine2.dz>
2020-03-10 11:23:13 +0100

    version initial

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from textblob import TextBlob
 from os import path as os_path
 from CreationModele import CreationModele
 from AnalyseModeles import AnalyseModeles
-
+""" module creation """
 cr = CreationModele()
 cr.lecture()
 cr.preTraitement()
 cr.creationModele()
-
+""" module analyse """
 analyse = AnalyseModeles("Python")
 analyse.lectureModee()
 analyse.filtrage()
