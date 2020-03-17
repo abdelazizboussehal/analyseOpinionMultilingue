358ee5aaff21b571f6b2c8bf58b224fe0b582964
Mohamed Raouf <yajuve.25.dz@gmail.com>
2020-03-17 19:47:06 +0100

    Créer une classe génération des modèles

--- library/generation_models.py
+++ library/generation_models.py
@@ -1,17 +1,74 @@
+from textblob import TextBlob
+from operator import itemgetter
+
 class GenerationModels():
 
-	def process(self):
+	def process(self, content):
+		""" Le processus du traitement """
+		blob = TextBlob(content)
+		tags = blob.tags
+		model = self.build_model(tags)
+		return model
 
 
-	def list_models(self):
+	def build_model(self, tags):
+		"""
+			Prendre les POS-tags comme paramètre et trouver le modèle correspondant puis le générer
+		"""
+		code_tags = list(map(itemgetter(1), tags)) # Gader les tags seulement
+		code_model = self.get_model(code_tags) # quel modèle on va choisir
+		subject = ''
+		opinion = ''
+
+		if 1 == code_model:
+			for tag in tags:
+				if tag[1] == 'NN':
+					subject = tag[0]
+				elif tag[1] == 'JJ':
+					opinion = tag[0]
+
+			model = self.build_model_one(subject, opinion)
+			return model
+
+
+	def get_model(self, tags):
+		""" Trouver le modèle correspondant à partir des POS-tag disponible """
+		if 'NN' in tags and 'JJ' in tags:
+			return 1
+		else:
+			return -1 # unknown
+
+
+	def models_parts(self):
+		""" Les éléments nécessaires pour chaque modèle """
 		models = {
-			"1": "Subject <- opinion(aspect)",
-			"2": "Subject <- opinion(aspect)@time",
-			"3": "Subject <- opinion(aspect)@time?holder",
+			"1": ["NN", "JJ"],
+			"2": [],
+			"3": [],
+			"4": [],
 			"n": "..."
 		}
 		return models
 
-	def model_one(self, subject, opinion, aspect):
-		return subject + "<-"  + opinion + "(" + aspect + ")"
+
+	def list_models(self):
+		""" Les modèles disponibles pour but d'affichage (Utilser une BDD pour les stocker par la suite) """
+		models = {
+			"1": "Subject <- opinion",
+			"2": "Subject <- opinion(aspect)",
+			"3": "Subject <- opinion(aspect)@time",
+			"4": "Subject <- opinion(aspect)@time?holder",
+			"n": "..."
+		}
+		return models
+
+
+	def build_model_one(self, subject, opinion):
+		""" Construction du premier modèle """
+		return subject + " <- "  + opinion
+
+
+	def build_model_two(self, subject, opinion, aspect):
+		""" Construction du deuxième modèle """
+		return subject + " <- "  + opinion + "(" + aspect + ")"
 
