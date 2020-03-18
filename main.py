52558353857f7e38d28cd93e86835b4966cd539d
boussehal abdelaziz <abdelaziz.boussehal@univ-constantine2.dz>
2020-03-18 13:14:08 +0100

    ajouter le module de CreationModele_fr.py

--- main.py
+++ main.py
@@ -1,30 +1,37 @@
 # -*- coding: utf-8 -*-
+'''
+Proposition d’un pivot interlangue (Ar-Fr-Ang) pour la traduction automatique afin d’aider à l’analyse d’opinion
+@group: Abdelaziz boussehal, Abderraouf Mahmoudi
+@date: 2019-2020
+'''
+
+from CreationModele import CreationModele
+from CreationModele_fr import CreationModele_fr
+from CreationModele_fr import CreationModele_fr
+from AnalyseModeles import AnalyseModeles
+from  CreationModles_ar import  CreationModeles_ar
+
+cr_fr = CreationModeles_ar()
+cr_fr.lecture("test_ar.txt")
+cr_fr.preTraitement()
+cr_fr.creationModele()
 """
-	Proposition d’un pivot interlangue (Ar-Fr-Ang) pour la traduction automatique afin d’aider à l’analyse d’opinion
-	@group: Abdelaziz boussehal, Abderraouf Mahmoudi
-	@date: 2019-2020
+cr_fr = CreationModele_fr()
+cr_fr.lecture("test_fr.txt")
+cr_fr.preTraitement()
+cr_fr.creationModele()
 """
-
-from library import generation_models as g
-from library import tools as t
-
-if __name__ == "__main__":
-
-	# Lecture du fichier
-	content = t.Tools.read("./data/test_en.txt")
-
-	# Instanciation du module Génération des modèles
-	generator = g.GenerationModels()
-
-	# Lancer le processuss de traitement
-	model = generator.process(content)
-
-	# Sauvegarder le modèle dans un fichier
-	t.Tools.write(model)
-
-	# Affichage dans la console
-	print("\n")
-	print("- Contenu du fichier : ",content)
-	print("- Modèle généré : ", model)
+""" module creation 
+cr = CreationModele()
+cr.lecture("test_en.txt")
+cr.preTraitement()
+cr.creationModele()
+"""
+""" module analyse 
+analyse = AnalyseModeles("Python")
+analyse.lectureModee()
+analyse.filtrage()
+print(analyse.analyse())
+"""
 
 
