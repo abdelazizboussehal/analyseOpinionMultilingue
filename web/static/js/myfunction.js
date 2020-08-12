a5605e5202c6d113a3d88eb9f129a1e989c98222
boussehal abdelaziz <abdelaziz.boussehal@univ-constantine2.dz>
2020-08-12 02:21:52 +0100

    * analyse modele global (lexicon englais lexicons français) dashboard version 4 session statistic

--- web/static/js/myfunction.js
+++ web/static/js/myfunction.js
@@ -58,6 +58,48 @@ function starprogress() {
     });
 }
 
+function statistic() {
+// Set new default font family and font color to mimic Bootstrap's default styling
+    Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+    Chart.defaults.global.defaultFontColor = '#858796';
+
+// Pie Chart Example
+    var ctx = document.getElementById("myPieChart");
+    var myPieChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: ["verbe", "adjective", "nouns"],
+            datasets: [{
+                data: [document.getElementById("nbr_verb").value,
+                    document.getElementById("nbr_adj").value,
+                    document.getElementById("nbr_nom").value],
+                backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
+                hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+                hoverBorderColor: "rgba(234, 236, 244, 1)",
+            }],
+        },
+        options: {
+            maintainAspectRatio: false,
+            tooltips: {
+                backgroundColor: "rgb(255,255,255)",
+                bodyFontColor: "#858796",
+                borderColor: '#dddfeb',
+                borderWidth: 1,
+                xPadding: 15,
+                yPadding: 15,
+                displayColors: false,
+                caretPadding: 10,
+            },
+            legend: {
+                display: false
+            },
+            cutoutPercentage: 80,
+        },
+    });
+    document.getElementById("statistique_cadre").hidden = false;
+
+}
+
 
 
 
